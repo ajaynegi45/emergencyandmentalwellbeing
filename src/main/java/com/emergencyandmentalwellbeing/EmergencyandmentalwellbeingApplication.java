0b84d5254048e4ead44dfeb87f9cbc5c69c59711
Ajay Negi <97536703+ajaynegi45@users.noreply.github.com>
2024-11-14 17:46:14 +0530

    completed initial setup

---
+++ src/main/java/com/emergencyandmentalwellbeing/EmergencyandmentalwellbeingApplication.java
@@ -0,0 +1,13 @@
+package com.emergencyandmentalwellbeing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyandmentalwellbeingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyandmentalwellbeingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emergencyandmentalwellbeing/EmergencyandmentalwellbeingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emergencyandmentalwellbeing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyandmentalwellbeingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
